c30fb021c2d79dacb38fe6c78e409fb03de561e9
Ashish T <ashish.tiwari@contino.io>
2023-02-04 23:43:04 +0000

    Create an UI Based App for Quizzing Time Tables

---
+++ quiz-test/chatgpt-quizV2.py
@@ -0,0 +1,64 @@
+import tkinter as tk
+from tkinter import messagebox
+from tkinter.ttk import *
+import random
+import time
+
+class QuizApp(tk.Tk):
+    def __init__(self, *args, **kwargs):
+        tk.Tk.__init__(self, *args, **kwargs)
+        self.reset_quiz()
+
+
+  
+    def create_widgets(self):
+        for i in range (self.total_questions):
+            num1 = random.randint(12, 15)
+            num2 = random.randint(2,12)
+            answer = num1*num2
+            self.real_answers.append(answer)
+            # grid method to arrange labels in respective
+            # rows and columns as specified
+            l1 = tk.Label(self, text = f"What is {num1} X {num2}: ")
+            l1.grid(row = i, column = 0)
+            l2 = tk.Label(self, text = "                                    ")
+            l2.grid(row = i, column = 3)
+            self.entry_answer.append(Entry(self))
+            self.entry_answer[i].delete(0, "end")
+            self.entry_answer[i].grid(row = i, column = 1)
+            
+        b = tk.Button(self, text = "Submit", command=self.check_answer)
+        b.grid(row = i+1, column=2)
+
+        b = tk.Button(self, text = "Close", command=self.close_app)
+        b.grid(row = i+1, column=1)
+
+
+    def check_answer(self):
+        for i in range (self.total_questions):
+            user_answer = int(self.entry_answer[i].get())
+            if  user_answer == self.real_answers[i]:
+                self.score += 1
+            
+            l2 = tk.Label(self, text = f"Correct Answer is {self.real_answers[i]} ")
+            l2.grid(row = i, column = 3)
+              
+        messagebox.showinfo(title = "Info", message = f"Quiz Score. Congratulations, You have completed the quiz! \n \
+                        Your score is {self.score} out of {self.total_questions} ")
+
+        b2 = tk.Button(self, text = "Reset", command=self.reset_quiz)
+        b2.grid(row = i+1, column=3)
+
+    def reset_quiz(self):
+        self.real_answers = []
+        self.user_answers =[]
+        self.score = 0
+        self.entry_answer = []
+        self.total_questions = 10
+        self.create_widgets()
+
+    def close_app(self):
+        self.destroy()
+   
+app = QuizApp()
+app.mainloop()
---
+++ quiz-test/multiplyV2.py
@@ -0,0 +1,65 @@
+import random
+import time
+def start_quiz:
+    num_questions = 5
+    min_num = 201
+    max_num = 999
+    time_limit = 45
+    time_taken = 0
+
+
+    number1 =[]
+    number2 = []
+    real_answers = []
+    user_answers =[]
+
+    start_time = time.time()
+    print(f"Start time is {start_time}")
+    score = 0
+    for i in range(num_questions):
+        num1 = random.randint(min_num, max_num)
+        num2 = random.randint(22, 99)
+        answer = num1 * num2
+        number1.append(num1)
+        number2.append(num2)
+        real_answers.append(answer)
+
+
+        time_taken = time.time() - start_time
+        if time_taken > time_limit:
+            break
+        user_answer = int(input(f"What is {num1} * {num2}? "))
+        
+        user_answers.append(user_answer)
+
+        if user_answer == answer:
+            print("Correct!")
+            score += 1
+        else:
+            print("Incorrect.")
+        print (f"Remaining Time: {time_limit-time_taken}")
+    total_time = (time.time() - start_time)
+    print(f"You scored {score}/{num_questions}.")
+    print(f"You have taken {total_time}. Could you try improving next time!")
+
+    print("See How did it\n")
+    print("NUMBER1 NUMBER2 RIGHT_ANSWER UR_ANSWER \n")
+
+    for i in range(num_questions):
+        print(f"{number1[i]}   X   {number2[i]}  =    {real_answers[i]}      {user_answers[i]}")
+
+window = tk.Tk()
+window.title("Multiplication Quiz")
+
+question_label = tk.Label(window, text="Welcome to the Multiplication Quiz!, \nEnter your name below", font=("TkDefaultFont", 14))
+question_label.pack()
+
+question_entry = tk.Entry(window, font=("TkDefaultFont", 14))
+question_entry.pack()
+
+start_button = tk.Button(window, text="Start Quiz", font=("TkDefaultFont", 14), command=start_quiz)
+start_button.pack()
+
+answer_button = tk.Button(window, text="OK", font=("TkDefaultFont", 14), command=calculate_score_time)
+
+window.mainloop()
